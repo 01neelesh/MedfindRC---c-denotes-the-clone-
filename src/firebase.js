090ff22afd0b4a2c48d2463e425a09fcaf542dda
neelesh chaturvedi <neeleshchaturvedi233@gmail.com>
2025-09-03 23:44:24 +0530

    import other firebase features

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,32 @@
 
 import { initializeApp } from "firebase/app";
-// Just remove the analytics import from here coz you don't need it
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 
-// after creating your app you must get a confi codes you just need to replace with below config 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey : process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const firestoreDB = getFirestore(app);
+export const storage = getStorage(app);
+export const functions = getFunctions(app);
+export const auth = getAuth(app);
+
+if (location.hostname === "localhost") {
+  connectFirestoreEmulator(firestoreDB, 'localhost', 8080);
+  connectStorageEmulator(storage, 'localhost', 9199);
+  connectFunctionsEmulator(functions, 'localhost', 5001);
+  connectAuthEmulator(auth, "http://localhost:9099");
+  console.log("🔥 Firebase Emulators connected");
+}
 
